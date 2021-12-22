91656805b1f05842aadee9b3c5574ee0f3cb7602
NadyaLenkovets <nadya.lenkovets@gmail.com>
2021-12-22 21:15:20 +0300

    add files

---
+++ js/dishes.js
@@ -0,0 +1,259 @@
+let dishes = `{ 
+   "hot-dishes" : [
+                     {
+                        "name": "Spicy seasoned seafood noodles",
+                        "price": "2.29",
+                        "amount" : "20",
+                        "image" : "hot dishes/1.png"
+                     },
+                     {
+                        "name": "Salted Pasta with mushroom sauce",
+                        "price": "2.69",
+                        "amount" : "11",
+                        "image" : "hot dishes/2.png"
+                     },
+                     {
+                        "name": "Beef dumpling in hot and sour soup",
+                        "price": "2.99",
+                        "amount" : "16",
+                        "image" : "hot dishes/3.png"
+                     },
+                     {
+                        "name": "Healthy noodle with spinach leaf",
+                        "price": "3.29",
+                        "amount" : "22",
+                        "image" : "hot dishes/4.png"
+                     },
+                     {
+                        "name": "Hot spicy fried rice with omelet",
+                        "price": "3.49",
+                        "amount" : "17",
+                        "image" : "hot dishes/5.png"
+                     },
+                     {
+                        "name": "Hot spicy fried rice with omelet",
+                        "price": "3.69",
+                        "amount" : "17",
+                        "image" : "hot dishes/6.png"
+                     },
+                     {
+                        "name": "Spicy noodles with shrimps and pepper",
+                        "price": "4.29",
+                        "amount" : "8",
+                        "image" : "hot dishes/1.png"
+                     },
+                     {
+                        "name": "Fried beef with spinach sause",
+                        "price": "4.59",
+                        "amount" : "10",
+                        "image" : "hot dishes/2.png"
+                     },
+                     {
+                        "name": "Beef dumpling with potatoes",
+                        "price": "3.99",
+                        "amount" : "9",
+                        "image" : "hot dishes/3.png"
+                     }
+                  ],
+   "cold-dishes" : [
+                     {
+                        "name": "Hummus with lemon flavour",
+                        "price": "1.29",
+                        "amount" : "10",
+                        "image" : "cold dishes/1.png"
+                     },
+                     {
+                        "name": "Hummus with mushrooms",
+                        "price": "1.69",
+                        "amount" : "11",
+                        "image" : "cold dishes/2.png"
+                     },
+                     {
+                        "name": "Georgian khachapuri with cheese",
+                        "price": "1.79",
+                        "amount" : "18",
+                        "image" : "cold dishes/3.png"
+                     },
+                     {
+                        "name": "Omelette with vegetables",
+                        "price": "1.19",
+                        "amount" : "21",
+                        "image" : "cold dishes/4.png"
+                     },
+                     {
+                        "name": "Fried rice with vegetables",
+                        "price": "1.19",
+                        "amount" : "19",
+                        "image" : "cold dishes/5.png"
+                     },
+                     {
+                        "name": "Cooked rice with garlic",
+                        "price": "1.09",
+                        "amount" : "24",
+                        "image" : "cold dishes/6.png"
+                     }
+                  ],
+   "soup" : [
+                     {
+                        "name": "Nodles with fried chicken",
+                        "price": "1.39",
+                        "amount" : "8",
+                        "image" : "soup/1.png"
+                     },
+                     {
+                        "name": "Spicy soup with mushrooms and vegetables",
+                        "price": "1.49",
+                        "amount" : "12",
+                        "image" : "soup/2.png"
+                     },
+                     {
+                        "name": "Coconut soup with shrimps",
+                        "price": "1.99",
+                        "amount" : "14",
+                        "image" : "soup/3.png"
+                     },
+                     {
+                        "name": "Noodles with shrimps and eggs",
+                        "price": "2.49",
+                        "amount" : "21",
+                        "image" : "soup/4.png"
+                     },
+                     {
+                        "name": "Vegetarian soup with broccoli",
+                        "price": "1.29",
+                        "amount" : "9",
+                        "image" : "soup/5.png"
+                     },
+                     {
+                        "name": "Creamy soup with chili and garlic",
+                        "price": "1.79",
+                        "amount" : "14",
+                        "image" : "soup/6.png"
+                     }
+                  ],
+   "grill" : [
+                     {
+                        "name": "Beef steak with garlic and rosemary",
+                        "price": "3.99",
+                        "amount" : "16",
+                        "image" : "grill/1.png"
+                     },
+                     {
+                        "name": "Chicken with honey marinade ",
+                        "price": "3.09",
+                        "amount" : "12",
+                        "image" : "grill/2.png"
+                     },
+                     {
+                        "name": "Chicken stripes with fried potatoes",
+                        "price": "2.89",
+                        "amount" : "10",
+                        "image" : "grill/3.png"
+                     },
+                     {
+                        "name": "Pizza with salami and olives",
+                        "price": "2.79",
+                        "amount" : "18",
+                        "image" : "grill/4.png"
+                     },
+                     {
+                        "name": "Beef steak with fried beans",
+                        "price": "4.29",
+                        "amount" : "9",
+                        "image" : "grill/5.png"
+                     },
+                     {
+                        "name": "Beef steak with pepper sauce",
+                        "price": "4.79",
+                        "amount" : "14",
+                        "image" : "grill/6.png"
+                     }
+                  ],
+   "appetizer" : [
+                     {
+                        "name": "Chicken with spinach and rice",
+                        "price": "2.39",
+                        "amount" : "11",
+                        "image" : "appetizer/1.png"
+                     },
+                     {
+                        "name": "Beef burger with barbeque sause",
+                        "price": "3.49",
+                        "amount" : "18",
+                        "image" : "appetizer/2.png"
+                     },
+                     {
+                        "name": "Barbeque chicken wings with sauce",
+                        "price": "2.99",
+                        "amount" : "9",
+                        "image" : "appetizer/3.png"
+                     },
+                     {
+                        "name": "Gyoza with chicken and tomato",
+                        "price": "2.59",
+                        "amount" : "14",
+                        "image" : "appetizer/4.png"
+                     },
+                     {
+                        "name": "Chicken with spicy garlic sause",
+                        "price": "2.79",
+                        "amount" : "19",
+                        "image" : "appetizer/5.png"
+                     },
+                     {
+                        "name": "Vareniki with mushrooms and cream",
+                        "price": "2.39",
+                        "amount" : "10",
+                        "image" : "appetizer/6.png"
+                     }
+                  ],
+   "dessert" : [
+                     {
+                        "name": "Chocolate fondante with strawberries",
+                        "price": "1.79",
+                        "amount" : "10",
+                        "image" : "dessert/1.png"
+                     },
+                     {
+                        "name": "Chocolate cake with whipped cream",
+                        "price": "2.09",
+                        "amount" : "10",
+                        "image" : "dessert/2.png"
+                     },
+                     {
+                        "name": "Coconut cake with whipped cream",
+                        "price": "2.49",
+                        "amount" : "13",
+                        "image" : "dessert/3.png"
+                     },
+                     {
+                        "name": "Strawberry cake with whipped cream",
+                        "price": "2.39",
+                        "amount" : "11",
+                        "image" : "dessert/4.png"
+                     },
+                     {
+                        "name": "Pancakes with honey and blueberry",
+                        "price": "2.99",
+                        "amount" : "11",
+                        "image" : "dessert/5.png"
+                     },
+                     {
+                        "name": "Pancakes with honey and raspberries",
+                        "price": "2.89",
+                        "amount" : "10",
+                        "image" : "dessert/6.png"
+                     }
+                  ]
+}`  
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/script.js
@@ -0,0 +1,380 @@
+const currentDate = document.querySelector('.current-date');
+const cards = document.querySelector('.cards');
+const searchInput = document.querySelector('.search-input');
+const searchError = document.getElementById('error');
+const cartButton = document.querySelector('.cart__btn');
+const frame = document.querySelector('.frame');
+const cartBlock = document.querySelector('.cart-block');
+const payment = document.querySelector('.payment');
+const confirmBtn = document.querySelector('.confirm__btn');
+const popup = document.querySelector('.popup');
+const popupClose = document.querySelector('.popup-close');
+const paymentMethodItems = document.querySelectorAll('.payment-method__item');
+const cart = document.querySelector('.cart');
+const subTotalSum = document.querySelector('.sub-total__sum');
+const backArrow = document.querySelector('.cart-block__back-arrow');
+const cancelButton = document.querySelector('.cancel__btn');
+
+let navListItems = document.querySelectorAll('.nav-list__item');
+let tabNavItems = document.querySelectorAll('.tabs-nav__item');
+let collection = document.getElementsByClassName('card'); // коллекция cards
+let cartAmountInputs = document.getElementsByClassName('cart__amount-input'); // коллекция amountInputs
+let cartItemsDelete = document.getElementsByClassName('cart-item__delete'); // коллекция deleteCartItem
+let cartItemSums = document.getElementsByClassName('cart-item__sum');  // коллекция cartItemSums
+
+//  ================  NAV   ================  //
+
+navListItems.forEach((navListItem) => {
+   navListItem.addEventListener('click', function() {
+      navListItems.forEach((navListItem) => {
+         navListItem.classList.remove('active');
+      })
+
+      this.classList.add('active');
+   })
+})
+
+
+
+//  ================  CURRENT DATE   ================  //
+
+let today = new Date();
+let year = today.getFullYear();
+let month = today.getMonth();
+let day = today.getDay();
+let date = today.getDate();
+let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thirsday', 'Friday', 'Saturday'];
+
+currentDate.innerHTML = days[day] + ', ' + date + ' ' +  months[month] + ' ' + year;
+
+
+
+//  ==========  JSON  ==========  //
+
+let dishesObj = JSON.parse(dishes);
+
+// текущий массив
+let currentArr = dishesObj['hot-dishes'];
+
+
+
+//  ================  TABS NAV   ================  //
+
+tabNavItems.forEach((tabNavItem) => {
+   tabNavItem.addEventListener('click', function() {
+      tabNavItems.forEach((tabNavItem) => {
+         tabNavItem.classList.remove('active');
+      })
+
+      this.classList.add('active');
+      searchInput.value = ''; // обнуляем поиск при переключении таба
+   })
+   tabNavItem.addEventListener('click', getCategoryName);
+})
+
+
+
+function getCategoryName() {
+   let category = this.dataset.tab;
+   currentArr = dishesObj[`${category}`];
+  
+   createCards(currentArr);
+   collection = document.getElementsByClassName('card');
+   collectionClick();
+}
+
+
+
+
+//  ================  CREATE CARDS   ================  //
+
+
+function createCards(currentArr) {
+   cards.innerHTML = '';
+
+   currentArr.forEach((elem) => {
+      let cardTemplate = `
+         <div class="card">
+            <div class="card__image">
+               <img src="img/dishes/${elem['image']}" alt="">
+            </div>
+            <div class="card__name">${elem['name']}</div>  
+            <div class="card__price">$ ${elem['price']}</div>  
+            <div class="card__amount">${elem['amount']} Bowls available</div>  
+         </div>
+      `;
+
+      cards.insertAdjacentHTML('beforeend', cardTemplate);
+   })
+}
+
+createCards(currentArr);
+
+
+
+
+//  ================  SEARCH   ================  //
+
+searchInput.addEventListener('input', function() {
+   let searchValue = searchInput.value.toLowerCase();
+   let counter = 0;
+   for (let elem of collection) {
+      elem.classList.remove('hidden');
+   }
+
+   for (let elem of collection) {
+      let dishName = elem.querySelector('.card__name').innerHTML.toLowerCase();
+      if (!dishName.includes(searchValue)) {
+         elem.classList.add('hidden');
+         counter++;
+      } else if (searchValue == '') {
+         elem.classList.remove('hidden');
+      }
+
+      if (counter == collection.length) {
+         error.classList.add('show');
+         setTimeout(hideError, 2000);
+      }
+   }
+})
+
+function hideError() {
+   error.classList.remove('show');
+}
+
+
+
+
+//  ================  CART BUTTON   ================  //
+
+cartButton.addEventListener('click', function() {
+   frame.classList.add('hidden');
+   payment.classList.add('active');
+   cartBlock.classList.add('active');
+})
+
+
+
+
+// ============  DROPDOWN CUSTOMIZATION  ============  //
+
+const multiDefault = () => {
+   const select = document.querySelector('#type-select');
+   const choices = new Choices(select, {
+      searchEnabled: false,
+      itemSelectText: '',
+   });
+};
+
+multiDefault();
+ 
+
+
+
+// ============  POPUP  ============  //
+
+confirmBtn.addEventListener('click', function() {
+   popup.classList.add('open');
+})
+
+popup.addEventListener("click", function(e) {
+   if (!e.target.closest('.popup-inner') || e.target == popupClose) {
+      this.classList.remove('open');
+   }
+})
+
+
+
+
+// ============  PAYMENT METHOD ITEMS  ============  //
+
+paymentMethodItems.forEach((paymentMethodItem) => {
+   paymentMethodItem.addEventListener('click', function() {
+
+      paymentMethodItems.forEach((paymentMethodItem) => {
+         paymentMethodItem.classList.remove('active');
+      })
+
+      this.classList.add('active');
+   })
+})
+
+
+
+// ============  COLLECTION CLICK  ============  //
+
+function collectionClick() {
+   for (let dishCard of collection) {
+      dishCard.addEventListener('click', function() {
+
+         let dishName = this.querySelector('.card__name').innerHTML;
+         let dishPrice = this.querySelector('.card__price').innerHTML;
+         let dishImage = this.querySelector('.card__image img').src;
+         
+         let dishTemplate = `
+            <div class="cart-item">
+               <div class="cart-item__content">
+                  <div class="cart-item__image">
+                     <img src="${dishImage}">
+                  </div>
+                  <div class="cart-item__block">
+                     <div class="cart-item__name">${dishName}</div>
+                     <div class="cart-item__price">${dishPrice}</div>
+                  </div>
+                  <div class="cart-item__amount">
+                     <input class="cart__amount-input" type="text" value="1">
+                  </div>
+                  <div class="cart-item__sum">$ ${countSum(convertPrice(dishPrice), 1)}</div>
+               </div>
+               <div class="cart-item__info">
+                  <div class="cart-item__comment">
+                     <input class="cart__comment-input" type="text" placeholder="Order Note...">
+                  </div>
+                  <div class="cart-item__delete">
+                     <svg class="cart-item__delete-icon">
+                        <use xlink:href="#delete"></use>
+                     </svg>
+                  </div>
+               </div>
+            </div>
+         `;
+
+         cart.insertAdjacentHTML('beforeend', dishTemplate);
+         activateAmountInputs();
+         activateCartItemsDelete();
+         subTotalCount();
+      });
+   }
+}
+
+collectionClick();
+
+
+function convertPrice(string) {
+   return result = +string.substr(2);
+}
+
+function countSum(price, number) {
+   return sum = price * number;
+}
+
+
+
+// ============  AMOUNT INPUTS  ============  //
+
+function activateAmountInputs() {
+   cartAmountInputs = document.getElementsByClassName('cart__amount-input');
+   for (let cartAmountInput of cartAmountInputs) {
+      cartAmountInput.addEventListener('input', function() {
+         let price = convertPrice(this.parentElement.previousElementSibling.lastElementChild.innerHTML);
+         let newSum = (this.value * price).toFixed(2);
+
+         this.parentElement.nextElementSibling.innerHTML = `$ ${newSum}`;
+         subTotalCount();
+      })
+   };
+}
+
+
+// ============  DELETE ITEMS  ============  //
+
+function activateCartItemsDelete() {
+   cartItemsDelete = document.getElementsByClassName('cart-item__delete');
+   for (let cartItemDelete of cartItemsDelete) {
+      cartItemDelete.addEventListener('click', function() {
+         this.parentElement.parentElement.remove();
+         subTotalCount();
+      })
+      
+   };
+   
+};
+
+
+
+// ============  SUB TOTAL COUNT  ============  //
+
+function subTotalCount() {
+   cartItemSums = document.getElementsByClassName('cart-item__sum');
+   let totalSum = 0;
+   for (let cartItemSum of cartItemSums) {
+      totalSum += convertPrice(cartItemSum.innerHTML);
+   }
+   subTotalSum.innerHTML = `$ ${totalSum.toFixed(2)}`;
+};
+
+subTotalCount();
+
+
+
+// ============  BACK ARROW, CANCEL BUTTON  ============  //
+
+backArrow.addEventListener('click', closePayment);
+cancelButton.addEventListener('click', closePayment);
+
+function closePayment() {
+   frame.classList.remove('hidden');
+   payment.classList.remove('active');
+   cartBlock.classList.remove('active');
+}
+
+
+
+
+
+// ============  INPUT MASKS  ============  //
+
+// ============  name  ============  //
+
+let name = document.querySelector('#name');
+let maskOptionsName = {
+  mask: /^[A-Za-z -]+$/
+};
+mask = IMask(name, maskOptionsName);
+
+// ============  card number  ============  //
+
+let cardNumber = document.querySelector('#card-number');
+let maskOptions = {
+  mask: '0000 0000 0000 0000',
+  lazy: false,
+  placeholderChar: '_'
+};
+mask = IMask(cardNumber, maskOptions);
+
+
+// ============  expiration-date  ============  //
+
+let expirationDate = document.querySelector('#expiration-date');
+let maskOptionsExpDate = {
+   mask: 'MM{/}YYYY',
+   lazy: false,
+   placeholderChar: '_',
+
+   blocks: {
+      YYYY: {
+         mask: '0000',
+         mask: IMask.MaskedRange,
+         from: 2021,
+         to: 2050
+      },
+
+      MM: {
+         mask: IMask.MaskedRange,
+         from: 1,
+         to: 12
+      },
+   }
+};
+mask = IMask(expirationDate, maskOptionsExpDate);
+
+
+// ============  password  ============  //
+
+let password = document.querySelector('#cvv');
+let maskOptionsPassword = {
+  mask: '000',
+};
+mask = IMask(password, maskOptionsPassword);
+
